cb7f5dda8ef0908799193ca25e3e8ee69877d2e1
adit-codes <aditirenuprakash@gmail.com>
2026-06-17 14:54:46 +0530

    Add files via upload

---
+++ rev_num.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        const int mx=INT_MAX/10,mn=INT_MIN/10;
+        int rev=0,last_dig=0;
+	    {
+            while(x!=0)
+	        {
+                last_dig=x%10;
+		        x=x/10;
+                if(rev > mx || rev < mn){return 0;}
+                rev=(rev*10)+last_dig;
+	        }
+	        return rev;
+        }    
+    }
+};   
