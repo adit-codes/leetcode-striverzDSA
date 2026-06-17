dc254fb375c4c3387fd84fc0d30383be159ba005
adit-codes <aditirenuprakash@gmail.com>
2026-06-17 15:22:10 +0530

    pallindrome.cpp

--- pallindrome.cpp
+++ pallindrome.cpp
@@ -6,19 +6,19 @@ public:
         const int mx=INT_MAX/10;
         const int mn=INT_MIN/10;
         int rev=0,last_dig=0;
-        {
+	    {
             while(x!=0)
-            {
+	        {
                 last_dig=x%10;
-                x=x/10;
+		        x=x/10;
                 if(rev > mx || rev < mn){return 0;}
                 rev=(rev*10)+last_dig;
-            }
-            return rev;
+	        }
+	        return rev;
         }    
     }
     bool isPalindrome(int x) {
-        if(x>=0){if(x==reverse(x)) return true;else return false;}
+        if(x>=0 && x==reverse(x)) return true;
         else return false;
     }
 };
